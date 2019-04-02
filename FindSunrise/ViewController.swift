5ae066791ef4d880bc8c5cd3345c4bff39a24994
omar uriel R.C <omaruriel181@gmail.com>
2019-04-02 17:01:38 -0600

    FindSunrise

--- FindSunrise/ViewController.swift
+++ FindSunrise/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tvShowSunriseTime: UILabel!
+    @IBOutlet weak var txtCityName: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buFindSunRise(_ sender: Any) {
+    }
+    
 }
 
